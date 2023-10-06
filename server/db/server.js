6958c0e7d1ee3ca3ef8044b883cb1e073383932f
Onur Eredo <onureredo@yahoo.com>
2023-10-06 12:31:00 +0200

    readme

--- server/db/server.js
+++ server/db/server.js
@@ -6,7 +6,6 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 });
 
